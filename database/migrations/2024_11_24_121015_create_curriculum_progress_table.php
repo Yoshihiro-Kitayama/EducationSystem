ebdbf824b701404bd6d73991d605a897ed81600b
Yoshihiro-Kitayama <gidai0726@gmail.com>
2024-11-24 21:51:12 +0900

    feature/Kitayama

---
+++ database/migrations/2024_11_24_121015_create_curriculum_progress_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curriculum_progress', function (Blueprint $table) {
+            $table->id();
+            $table->integer('curriculumus_id');
+            $table->integer('users_id');
+            $table->tinyInteger('clear_flg')->default(0);
+            $table->timestamps();
+
+            // マイグレーション実行のために一時コメントアウト。
+            // curriculumsテーブルをpullできた後でコメントアウトは解除。
+            // $table->foreign('curriculumus_id')->references('id')->on('curriculums');
+
+            $table->foreign('users_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curriculum_progress');
+    }
+};
